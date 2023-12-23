0e693a1e95bfdc70628996406711dd9d28d384b1
Ahmed Hannan <ahmedhannansidra2@hotmail.com>
2023-12-23 12:25:51 +0500

    Add functionality to type numbers and operators

--- script.js
+++ script.js
@@ -1,6 +1,70 @@
-let num1 = 0;
-let num2 = 0;
-let operator = "";
+let numList = []
+
+// DOM Elements
+const display = document.querySelector("#display-text");
+
+const clearBtn = document.querySelector("#clear");
+
+const plusBtn = document.querySelector("#plus");
+const minusBtn = document.querySelector("#minus");
+const multiplyBtn = document.querySelector("#multiply");
+const divideBtn = document.querySelector("#divide");
+
+const oneBtn = document.querySelector("#one");
+const twoBtn = document.querySelector("#two");
+const threeBtn = document.querySelector("#three");
+const fourBtn = document.querySelector("#four");
+const fiveBtn = document.querySelector("#five");
+const sixBtn = document.querySelector("#six");
+const sevenBtn = document.querySelector("#seven");
+const eightBtn = document.querySelector("#eight");
+const nineBtn = document.querySelector("#nine");
+
+clearBtn.addEventListener("click", () => {
+    numList.splice(0, numList.length);
+    updateDisplay("0");
+});
+
+plusBtn.addEventListener("click", () => handleOperatorClick("+"));
+minusBtn.addEventListener("click", () => handleOperatorClick("-"));
+multiplyBtn.addEventListener("click", () => handleOperatorClick("*"));
+divideBtn.addEventListener("click", () => handleOperatorClick("/"));
+
+oneBtn.addEventListener("click", () => handleNumberClick(1));
+twoBtn.addEventListener("click", () => handleNumberClick(2));
+threeBtn.addEventListener("click", () => handleNumberClick(3));
+fourBtn.addEventListener("click", () => handleNumberClick(4));
+fiveBtn.addEventListener("click", () => handleNumberClick(5));
+sixBtn.addEventListener("click", () => handleNumberClick(6));
+sevenBtn.addEventListener("click", () => handleNumberClick(7));
+eightBtn.addEventListener("click", () => handleNumberClick(8));
+nineBtn.addEventListener("click", () => handleNumberClick(9));
+
+function handleNumberClick(num) {
+    if (!isNaN(parseInt(numList[numList.length - 1]))) {
+        numList[numList.length - 1] = parseInt(numList[numList.length - 1].toString() + num);
+    } else {
+        numList.push(num);
+    }
+    updateDisplay(numList.join(" "));
+}
+
+function handleOperatorClick(oper) {
+    if (
+        numList.length != 0 &&
+        numList[numList.length - 1] !== "+" &&
+        numList[numList.length - 1] !== "-" &&
+        numList[numList.length - 1] !== "*" &&
+        numList[numList.length - 1] !== "/"
+    ) {
+        numList.push(oper);
+    }
+    updateDisplay(numList.join(" "));
+}
+
+function updateDisplay(text) {
+    display.textContent = text;
+}
 
 function operate(a, b, operator) {
     switch (operator) {
@@ -20,6 +84,7 @@ function operate(a, b, operator) {
             result = "ERROR";
     }
     
+    updateDisplay(result);
     return result;
 }
 
